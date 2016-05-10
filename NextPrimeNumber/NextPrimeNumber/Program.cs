e5622725a7af41a302a730c858cf0bfcdf9e90bc
Hussen Fakhrulddin <hf556482@gmail.com>
2016-05-09 22:28:05 -0700

    init

---
+++ NextPrimeNumber/NextPrimeNumber/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace NextPrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Find the next prime number");
+                int number = int.Parse(Console.ReadLine());
+                Console.WriteLine(Calculation(number));
+            }
+        }
+
+        public static int Calculation(int number)
+        {
+            while (true)
+            {
+                bool isPrime = true;
+                //increment the number by 1 each time
+                number = number + 1;
+
+                int squaredNumber = (int)Math.Sqrt(number);
+
+                //start at 2 and increment by 1 until it gets to the squared number
+                for (int i = 2; i <= squaredNumber; i++)
+                {
+                    //how do I check all i's?
+                    if (number % i == 0)
+                    {
+                        isPrime = false;
+                        break;
+                     
+                    }
+                }
+                if (isPrime)
+                    return number;
+            }
+        }
+    }
+}
